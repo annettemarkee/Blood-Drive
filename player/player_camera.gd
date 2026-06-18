50da1969081f182b0b3a5c40cd609f500aaa796e
Annie Markee <annettemarkee@gmail.com>
2026-06-18 14:09:40 -0600

    Functional camera pivot via mouse

---
+++ player/player_camera.gd
@@ -0,0 +1,18 @@
+extends Marker3D
+
+@export var mouse_sensitivity := 0.005
+@export var min_pitch := -0.5
+@export var max_pitch := 0.8
+
+func _ready() -> void:
+	# Hide and capture the mouse
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		# Rotate horizontally (around the player)
+		rotation.y -= event.relative.x * mouse_sensitivity
+		
+		# Rotate vertically (pitch)
+		rotation.x -= event.relative.y * mouse_sensitivity
+		rotation.x = clamp(rotation.x, min_pitch, max_pitch)
